51d209e76a6a30795ed9c3438e5eed52781418a9
andrelimaiftm <36212719+andrelimaiftm@users.noreply.github.com>
2022-04-26 14:31:05 -0300

    Add files via upload

---
+++ PetShop/Caminhada.java
@@ -0,0 +1,8 @@
+public class Caminhada {
+
+    void andar(int x, int y){        
+        int resultado = x + y;
+        System.out.println("O resultado é: " + resultado);
+    }   
+    
+}
---
+++ PetShop/PetShop.java
@@ -0,0 +1,43 @@
+public class PetShop {
+
+    public static void main(String[] args) {
+
+        Caminhada caminhada = new Caminhada();
+        //int x = 3;
+        //int y = 2;
+        caminhada.andar(3, 2);
+        
+        /*Cachorro c1 = new Cachorro();
+        c1.nome = "Lumbriga";
+        c1.raca = "linguiça";
+        c1.cor = "caramelo";
+        c1.sexo = 'F';
+        c1.idade = 2;
+
+        Cachorro c2 = new Cachorro();
+
+        Gato g1 = new Gato();
+        g1.nome = "Linguado";
+        g1.raca = "Siames";
+        g1.sexo = 'M';
+        g1.cor = "Amarelo";
+        g1.idade = 1;
+
+        System.out.println("Objeto c1");
+        System.out.println(c1.nome);
+        System.out.println(c1.raca);
+        System.out.println();
+
+        System.out.println("Objeto c2");
+        System.out.println(c2.nome);
+        System.out.println(c2.raca);
+        System.out.println();
+
+        System.out.println("Objeto g1");
+        System.out.println(g1.nome);
+        System.out.println(g1.raca);*/
+
+
+    }
+    
+}
